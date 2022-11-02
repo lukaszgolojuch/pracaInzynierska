532471b7eb5085436510351d83a1a0dab4071638
lukaszgolojuch <lukasz.golojuch@gmail.com>
2022-11-02 18:31:53 +0100

    Initial Commit

---
+++ pracaInzynierska/pracaInzynierskaApp.swift
@@ -0,0 +1,17 @@
+//
+//  pracaInzynierskaApp.swift
+//  pracaInzynierska
+//
+//  Created by Łukasz Gołojuch on 11/2/22.
+//
+
+import SwiftUI
+
+@main
+struct pracaInzynierskaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
