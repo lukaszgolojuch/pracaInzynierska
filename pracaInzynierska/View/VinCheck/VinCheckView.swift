fce3dc56587fad33e80a4a2fe60ea5a957ae5927
lukaszgolojuch <lukasz.golojuch@gmail.com>
2022-11-03 23:16:08 +0100

    Implement CheckVinView

--- pracaInzynierska/View/VinCheck/VinCheckView.swift
+++ pracaInzynierska/View/VinCheck/VinCheckView.swift
@@ -8,8 +8,36 @@
 import SwiftUI
 
 struct VinCheckView: View {
+    @State private var newItemName: String = ""
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack{
+            HStack{
+                TextField("VIN number", text: $newItemName)
+                    .font(.custom(
+                            "Helvetica Neue",
+                            fixedSize: 30))
+            }
+            .padding(.horizontal, 30).padding(.top, 30)
+            
+            Divider()
+                .frame(height: 1)
+                .padding(.horizontal, 30)
+                .background(.blue)
+            
+            HStack{
+                Spacer()
+                Button("Decode") {
+                    print("Button pressed!")
+                }
+                .padding()
+                .background(.blue)
+                .foregroundColor(.white)
+                .clipShape(Capsule())
+            }
+            
+            Spacer()
+        }.padding()
     }
 }
 
